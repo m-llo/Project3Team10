edbcc9fb9c02715269c389fcf5f8a5302539ab17
Michael Jones <mtjones1979@yahoo.com>
2021-05-29 12:34:29 -0500

    trying to fix bugs

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,15 @@
 
 import axios from "axios";
+const BASEURL = "https://api.edamam.com/search?q="
+const APIKEY = "&app_id=40e21d0a&app_key=d32282d44a926244791002337e579dc9&count=10"
+
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  searchRecipes: function(q){
+    return axios.get(BASEURL + q + APIKEY)
+  },
   getRecipesByQuery: function(q) {
 
     return axios.get("/api/recipes/" + q);
