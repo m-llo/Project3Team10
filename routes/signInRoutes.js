07f6d83df32a22e363b66b9f899ce53c5278ac9a
Shaquille Oriol <shaquille.oriol@gmail.com>
2021-06-07 20:32:56 -0500

    new changes

--- routes/signInRoutes.js
+++ routes/signInRoutes.js
@@ -1,48 +1,48 @@
-const express = require('express')
-const router = express.Router()
-const signUpTemplateCopy = require('../models/User.js')
-const bcrypt = require('bcrypt')
+// const express = require('express')
+// const router = express.Router()
+// const signUpTemplateCopy = require('../models/User.js')
+// const bcrypt = require('bcrypt')
 
-router.post('/new', async (request, response) =>{
-    console.log(request.body)
-    const saltPassword = await bcrypt.genSalt(10)
-    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+// router.post('/new', async (request, response) =>{
+//     console.log(request.body)
+//     const saltPassword = await bcrypt.genSalt(10)
+//     const securePassword = await bcrypt.hash(request.body.password, saltPassword)
 
-    const signedUpUser = new signUpTemplateCopy({
+//     const signedUpUser = new signUpTemplateCopy({
        
-       username: request.body.username,
-       email: request.body.email,
-       password: securePassword,
+//        username: request.body.username,
+//        email: request.body.email,
+//        password: securePassword,
        
-    })
-    signedUpUser.save()
-    .then(data =>{
-        response.json(data)
-    })
-    .catch(error =>{
-        response.json(error)
-    })
-})
+//     })
+//     signedUpUser.save()
+//     .then(data =>{
+//         response.json(data)
+//     })
+//     .catch(error =>{
+//         response.json(error)
+//     })
+// })
 
-router.post('/signin', async (request, response) =>{
-    console.log(request.body)
-    const saltPassword = await bcrypt.genSalt(10)
-    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+// router.post('/signin', async (request, response) =>{
+//     console.log(request.body)
+//     const saltPassword = await bcrypt.genSalt(10)
+//     const securePassword = await bcrypt.hash(request.body.password, saltPassword)
 
-    const signInUser = new signUpTemplateCopy({
+//     const signInUser = new signUpTemplateCopy({
        
-       email: request.body.email,
-       password: securePassword,
+//        email: request.body.email,
+//        password: securePassword,
        
-    })
-    signInUser.save()
-    .then(data =>{
-        response.json(data)
-    })
-    .catch(error =>{
-        response.json(error)
-    })
-})
+//     })
+//     signInUser.save()
+//     .then(data =>{
+//         response.json(data)
+//     })
+//     .catch(error =>{
+//         response.json(error)
+//     })
+// })
 
 
-module.exports = router
+// module.exports = router
