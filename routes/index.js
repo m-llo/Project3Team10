ca52e8e121254d19c9f986a45b7ffc92297505fe
m-llo <markalloyd21@gmail.com>
2021-06-07 19:25:47 -0500

    set up back end route framework

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const signInRoutes = require("./signInRoutes")
+
 
 // API Routes
 router.use("/api", apiRoutes);
-router.use("/signup", signInRoutes);
+// router.use("/signup", signInRoutes);
 
 // If no API routes are hit, send the React app
 // router.use(function(req, res) {
