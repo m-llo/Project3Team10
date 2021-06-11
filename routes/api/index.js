02d302b06ff42b6c3ea65e231daf210836cc0e8c
Shaquille Oriol <shaquille.oriol@gmail.com>
2021-06-10 21:07:08 -0500

    List pages/routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,13 @@ const calendarRoutes = require("./calendarRoutes");
 const recipeRoutes = require("./recipeRoutes");
 const logInRoutes = require("./logInRoutes");
 const signUpRoutes = require("./signUpRoutes");
+const listRoutes = require("./listRoutes");
 
 // Post routes
 router.use("/recipes", recipeRoutes);
 router.use("/calendar", calendarRoutes);
 router.use("/signup", signUpRoutes);
 router.use("/user", logInRoutes);
+router.use("/list", listRoutes);
 
 module.exports = router;
--- routes/api/listRoutes.js
+++ routes/api/listRoutes.js
@@ -1,7 +1,17 @@
+const router = require("express").Router();
+const db = require("../../models");
+const listController = require("../../controllers/listController");
+
+router 
+.route ("/")
+.post(listController.create)
+
+router
+.route("/:id")
+.get(listController.findById)
+.put(listController.update)
+.delete(listController.remove)
 
 
 
-
-
-
-
+module.exports = router;
