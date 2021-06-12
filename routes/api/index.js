4717127d7d3347992d720bd85a426171ab46ddb9
m-llo <markalloyd21@gmail.com>
2021-06-12 12:43:28 -0500

    deploying again

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,11 +6,15 @@ const signUpRoutes = require("./signUpRoutes");
 const listRoutes = require("./listRoutes");
 
 
+
 // Post routes
 router.use("/recipes", recipeRoutes);
 router.use("/calendar", calendarRoutes);
 router.use("/signup", signUpRoutes);
 router.use("/user", logInRoutes);
 router.use("/list", listRoutes);
+router.use("/", logInRoutes);
+
+
 
 module.exports = router;
