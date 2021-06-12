54a67b0f1ed20391edca9441a742a0d16c6a9b7f
Michael T Jones <mtjones1979@yahoo.com>
2021-06-11 21:58:42 -0500

    Revert "updated node_module"

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../../assets/images/cÜk_long.png"
 
 const login = () => {
-    window.location.href = '/login';
+    window.location.href = '/signup';
 }
 const logout = () => {
     localStorage.clear();
